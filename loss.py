ac5ebdf27003c389762bdab90a85f2fbb00d3c4b
Gromiusz <104679774+Gromiusz@users.noreply.github.com>
2024-05-31 21:46:00 +0200

    Update loss.py

--- loss.py
+++ loss.py
@@ -5,7 +5,7 @@ from utils import intersection_over_union
 
 class YoloLoss(nn.Module): # wyzanczenie straty
 
-    def __init__(self, S=7, B=2, C=20):
+    def __init__(self, S=7, B=2, C=50): # Zmieniono na C=50
         super(YoloLoss, self).__init__()
         self.mse = nn.MSELoss(reduction="sum")
 
@@ -20,21 +20,21 @@ class YoloLoss(nn.Module): # wyzanczenie straty
         # (BATCH_SIZE, S*S(C+B*5)
         predictions = predictions.reshape(-1, self.S, self.S, self.C + self.B * 5)
 
-        iou_b1 = intersection_over_union(predictions[..., 21:25], target[..., 21:25])
-        iou_b2 = intersection_over_union(predictions[..., 26:30], target[..., 21:25])
+        iou_b1 = intersection_over_union(predictions[..., self.C+1:self.C+5], target[..., self.C+1:self.C+5])
+        iou_b2 = intersection_over_union(predictions[..., self.C+6:self.C+10], target[..., self.C+1:self.C+5])
         ious = torch.cat([iou_b1.unsqueeze(0), iou_b2.unsqueeze(0)], dim=0)
 
         iou_maxes, bestbox = torch.max(ious, dim=0) # wybór boxa z największym IoU
-        exists_box = target[..., 20].unsqueeze(3) 
+        exists_box = target[..., self.C].unsqueeze(3) 
 
         box_predictions = exists_box * (
             (
-                bestbox * predictions[..., 26:30]
-                + (1 - bestbox) * predictions[..., 21:25]
+                bestbox * predictions[..., self.C+6:self.C+10]
+                + (1 - bestbox) * predictions[..., self.C+1:self.C+5]
             )
         )
 
-        box_targets = exists_box * target[..., 21:25]
+        box_targets = exists_box * target[..., self.C+1:self.C+5]
 
         box_predictions[..., 2:4] = torch.sign(box_predictions[..., 2:4]) * torch.sqrt(
             torch.abs(box_predictions[..., 2:4] + 1e-6)
@@ -47,34 +47,27 @@ class YoloLoss(nn.Module): # wyzanczenie straty
         )
 
         pred_box = (
-            bestbox * predictions[..., 25:26] + (1 - bestbox) * predictions[..., 20:21]
+            bestbox * predictions[..., self.C+5:self.C+6] + (1 - bestbox) * predictions[..., self.C:self.C+1]
         )
 
         object_loss = self.mse(
             torch.flatten(exists_box * pred_box),
-            torch.flatten(exists_box * target[..., 20:21]),
+            torch.flatten(exists_box * target[..., self.C:self.C+1]),
         )
 
-        #max_no_obj = torch.max(predictions[..., 20:21], predictions[..., 25:26])
-        #no_object_loss = self.mse(
-        #    torch.flatten((1 - exists_box) * max_no_obj, start_dim=1),
-        #    torch.flatten((1 - exists_box) * target[..., 20:21], start_dim=1),
-        #)
-
         no_object_loss = self.mse(
-            torch.flatten((1 - exists_box) * predictions[..., 20:21], start_dim=1),
-            torch.flatten((1 - exists_box) * target[..., 20:21], start_dim=1),
+            torch.flatten((1 - exists_box) * predictions[..., self.C:self.C+1], start_dim=1),
+            torch.flatten((1 - exists_box) * target[..., self.C:self.C+1], start_dim=1),
         )
 
         no_object_loss += self.mse(
-            torch.flatten((1 - exists_box) * predictions[..., 25:26], start_dim=1),
-            torch.flatten((1 - exists_box) * target[..., 20:21], start_dim=1)
+            torch.flatten((1 - exists_box) * predictions[..., self.C+5:self.C+6], start_dim=1),
+            torch.flatten((1 - exists_box) * target[..., self.C:self.C+1], start_dim=1)
         )
 
-
         class_loss = self.mse(
-            torch.flatten(exists_box * predictions[..., :20], end_dim=-2,),
-            torch.flatten(exists_box * target[..., :20], end_dim=-2,),
+            torch.flatten(exists_box * predictions[..., :self.C], end_dim=-2),
+            torch.flatten(exists_box * target[..., :self.C], end_dim=-2),
         )
 
         loss = (
