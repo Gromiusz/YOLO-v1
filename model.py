455887fee6dd96fcf63a0d96ecc31a8334cd2019
Gromiusz <104679774+Gromiusz@users.noreply.github.com>
2024-06-09 22:31:20 +0200

    Update model.py

--- model.py
+++ model.py
@@ -96,12 +96,14 @@ class Yolov1(nn.Module):
 
         return nn.Sequential(
             # Oryginalnie
+            nn.Flatten(),
             nn.Linear(1024*S*S, 4096),
             nn.LeakyReLU(0.1),
             nn.Linear(4096, S*S*(B*5+C)),
-            nn.Flatten(),
-            nn.Linear(1024 * S * S, 496),
-            nn.Dropout(0.0),
-            nn.LeakyReLU(0.1),
-            nn.Linear(496, S * S * (C + B * 5)),
+            # nn.Flatten(),
+            # nn.Linear(1024 * S * S, 496),
+            # nn.Dropout(0.0),
+            # nn.LeakyReLU(0.1),
+            # nn.Linear(496, S * S * (C + B * 5)),
+            
         )
